169ae4893d01bf9d3d30f40038f2d6d8393d47e4
21279 <2127983518@qq.com>
2025-10-10 13:12:16 +0800

    树形聊天初始化

---
+++ src/test/java/com/example/treechat/TreeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
